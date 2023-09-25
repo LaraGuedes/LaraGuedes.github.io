a5cdefea004f1c3204bcdacd2fa06fde8b62d437
LaraGuedes <lara.guedes569@gmail.com>
2023-09-25 20:35:22 -0300

    criação das primeiras sessões, adicionando bootstrap

---
+++ script.js
@@ -0,0 +1,11 @@
+function alterarTemaSite() {
+    let tema = document.querySelector("html").getAttribute("data-bs-theme")
+    if(tema === "light"){
+        document.querySelector("html").setAttribute("data-bs-theme", "dark")
+        document.querySelector("#alterarTemaSite").innerHTML = `<i class="bi bi-moon-fill"></i>`
+    }
+    else{
+        document.querySelector("html").setAttribute("data-bs-theme", "light")
+        document.querySelector("#alterarTemaSite").innerHTML = `<i class="bi bi-sun-fill"> </i>`
+    }
+}
