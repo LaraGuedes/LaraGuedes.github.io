4d6409ece77d87e70075e154b044885c38039095
LaraGuedes <lara.guedes569@gmail.com>
2023-09-27 19:30:45 -0300

    adicionando os trabalhos no portifolio, certificados e iniciando o formulário

--- script.js
+++ script.js
@@ -11,4 +11,14 @@ function alterarTemaSite() {
     }
 }
 
+class formSubmit{
+    constructor(settings){
+        this.settings = settings;
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute("action")
+        }
+    }
 
+}
